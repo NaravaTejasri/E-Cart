d956a4d44e6593afbb6fe7015956a8d3e64561d2
NaravaTejasri <tejasrisony17@gmail.com>
2022-01-19 14:07:42 +0100

    created redux store for the user

---
+++ src/store/user/action.js
@@ -0,0 +1,42 @@
+import { apiUrl } from "../../config/constants";
+import axios from "axios";
+import {
+  appDoneLoading,
+  appLoading,
+  setMessage,
+  showMessageWithTimeout,
+} from "../appState/actions";
+
+export const loginSuccess = (userWithToken) => ({
+  type: "user/loginSuccess",
+  payload: userWithToken,
+});
+
+export const logOut = () => ({
+  type: "user/userLogout",
+});
+
+export const login = (email, password) => {
+  return async (dispatch, getState) => {
+    dispatch(appLoading());
+    try {
+      const response = await axios.post(`${apiUrl}/auth/login`, {
+        email,
+        password,
+      });
+      console.log(response.data);
+      dispatch(loginSuccess(response.data));
+      dispatch(showMessageWithTimeout("success", false, "welcome back!", 1500));
+      dispatch(appDoneLoading());
+    } catch (error) {
+      if (error.response) {
+        // console.log(error.response.data.message);
+        dispatch(setMessage("danger", true, error.response.data.message));
+      } else {
+        // console.log(error.message);
+        dispatch(setMessage("danger", true, error.message));
+      }
+      dispatch(appDoneLoading());
+    }
+  };
+};
---
+++ src/store/user/reducer.js
@@ -0,0 +1,23 @@
+const initialState = {
+  name: null,
+  email: null,
+  token: localStorage.getItem("token"),
+};
+
+export default function userReducer(state = initialState, action) {
+  switch (action.type) {
+    case "user/loginSuccess": {
+      console.log("user action", action.payload);
+      localStorage.setItem("token", action.payload.token);
+      return { ...state, ...action.payload };
+    }
+
+    case "user/userLogout":
+      localStorage.removeItem("token");
+      return { ...initialState, token: null };
+
+    default: {
+      return state;
+    }
+  }
+}
