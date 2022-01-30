b3bec5b9119ccf5fd312927321bc6499ca0bce4b
NaravaTejasri <tejasrisony17@gmail.com>
2022-01-30 20:43:25 +0100

    created component(CreateCategory) so admin can create new category

--- src/pages/Home/Categories.js
+++ src/pages/Home/Categories.js
@@ -4,6 +4,97 @@ import "../../styles/homepage.styles.scss";
 import Category from "../../components/Category/Category";
 import { fetchCategories } from "../../store/categories/action";
 import { selectCategories } from "../../store/categories/selector";
+import { Link } from "react-router-dom";
+import { selectUser } from "../../store/user/selector";
+
+function Categories() {
+  const categories = useSelector(selectCategories);
+  const { token, isAdmin } = useSelector(selectUser);
+
+  //console.log("categories", categories);
+  const [searchInput, setSearchInput] = useState("");
+  const [filteredResults, setFilteredResults] = useState([]);
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchCategories());
+  }, [dispatch]);
+
+  //search for category
+  const searchItems = (searchValue) => {
+    setSearchInput(searchValue);
+    if (searchInput !== "") {
+      const filteredData = categories.filter((item) => {
+        return (
+          item.title.toLowerCase().includes(searchInput.toLowerCase()) ||
+          item.title.toLowerCase().includes(searchInput.toLowerCase())
+        );
+      });
+      //console.log("filtered data", filteredData);
+      setFilteredResults(filteredData);
+    } else {
+      setFilteredResults(categories);
+    }
+  };
+
+  return (
+    <div className="homepage">
+      <div className="category">
+        <Link to="/createCategory">
+          {isAdmin ? <button>Create New</button> : null}
+        </Link>
+      </div>
+      <div>
+        <input
+          class="search"
+          type="search"
+          value={searchInput}
+          placeholder="Search...categories"
+          onChange={(e) => searchItems(e.target.value)}
+        />
+      </div>
+      <h3>Categories</h3>
+      <div key={categories.id} className="directory-menu">
+        {searchInput.length > 1 ? (
+          <>
+            {filteredResults.map((category) => (
+              <Category
+                key={category._id}
+                id={category.id}
+                title={category.title}
+                subtitle={category.subtitle}
+                imageUrl={category.imageUrl}
+              />
+            ))}
+          </>
+        ) : (
+          <>
+            {categories.map((category) => {
+              return (
+                <Category
+                  key={category.id}
+                  id={category.id}
+                  title={category.title}
+                  subtitle={category.subtitle}
+                  imageUrl={category.imageUrl}
+                />
+              );
+            })}
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Categories;
+
+/* import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import "../../styles/homepage.styles.scss";
+import Category from "../../components/Category/Category";
+import { fetchCategories } from "../../store/categories/action";
+import { selectCategories } from "../../store/categories/selector";
 
 function Categories() {
   const categories = useSelector(selectCategories);
@@ -21,6 +112,7 @@ function Categories() {
     setSearchInput(searchValue);
     if (searchInput !== "") {
       const filteredData = categories.filter((item) => {
+      if(item.title)
         return (
           item.title.toLowerCase().includes(searchInput.toLowerCase()) ||
           item.title.toLowerCase().includes(searchInput.toLowerCase())
@@ -78,4 +170,4 @@ function Categories() {
   );
 }
 
-export default Categories;
+export default Categories;*/
---
+++ src/pages/Home/CreateCategory.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { createCategory } from "../../store/categories/action";
+import "../../styles/login.styles.scss";
+
+function CreateCategory() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [title, setTitle] = useState("");
+  const [subtitle, setsubTitle] = useState("");
+  const [imageUrl, setImageUrl] = useState(
+    "https://source.unsplash.com/1600x900/?"
+  );
+
+  function submitForm(event) {
+    event.preventDefault();
+    console.log(title, subtitle, imageUrl);
+    dispatch(createCategory(title, subtitle, imageUrl));
+    navigate("/");
+  }
+
+  return (
+    <div className="sign-in">
+      <div className="container">
+        <h3>Create a new category</h3>
+        <form className="form">
+          <input
+            type="text"
+            value={title}
+            onChange={(event) => setTitle(event.target.value)}
+            placeholder="title name"
+            required
+          />
+          <input
+            type="text"
+            value={subtitle}
+            onChange={(event) => setsubTitle(event.target.value)}
+            placeholder="subtitle"
+            required
+          />
+          <input
+            value={imageUrl}
+            onChange={(event) => setImageUrl(event.target.value)}
+            type="text"
+            placeholder="https://"
+            required
+          ></input>
+          <button type="submit" class="btn" onClick={submitForm}>
+            create
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default CreateCategory;
