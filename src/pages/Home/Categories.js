bcb1f8256755503e977244c8f2b4b4ced45815bd
NaravaTejasri <tejasrisony17@gmail.com>
2022-01-30 12:14:41 +0100

    added search bar functionality

--- src/pages/Home/Categories.js
+++ src/pages/Home/Categories.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import "../../styles/homepage.styles.scss";
 import Category from "../../components/Category/Category";
@@ -8,25 +8,71 @@ import { selectCategories } from "../../store/categories/selector";
 function Categories() {
   const categories = useSelector(selectCategories);
   //console.log("categories", categories);
+  const [searchInput, setSearchInput] = useState("");
+  const [filteredResults, setFilteredResults] = useState([]);
+
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchCategories());
   }, [dispatch]);
+
+  //search for category
+  const searchItems = (searchValue) => {
+    setSearchInput(searchValue);
+    if (searchInput !== "") {
+      const filteredData = categories.filter((item) => {
+        return (
+          item.title.toLowerCase().includes(searchInput.toLowerCase()) ||
+          item.title.toLowerCase().includes(searchInput.toLowerCase())
+        );
+      });
+      //console.log("filtered data", filteredData);
+      setFilteredResults(filteredData);
+    } else {
+      setFilteredResults(categories);
+    }
+  };
+
   return (
     <div className="homepage">
+      <div>
+        <input
+          class="search"
+          type="search"
+          value={searchInput}
+          placeholder="Search...categories"
+          onChange={(e) => searchItems(e.target.value)}
+        />
+      </div>
       <h3>Categories</h3>
       <div key={categories.id} className="directory-menu">
-        {categories.map((category) => {
-          return (
-            <Category
-              key={category.id}
-              id={category.id}
-              title={category.title}
-              subtitle={category.subtitle}
-              imageUrl={category.imageUrl}
-            />
-          );
-        })}
+        {searchInput.length > 1 ? (
+          <>
+            {filteredResults.map((category) => (
+              <Category
+                key={category._id}
+                id={category.id}
+                title={category.title}
+                subtitle={category.subtitle}
+                imageUrl={category.imageUrl}
+              />
+            ))}
+          </>
+        ) : (
+          <>
+            {categories.map((category) => {
+              return (
+                <Category
+                  key={category.id}
+                  id={category.id}
+                  title={category.title}
+                  subtitle={category.subtitle}
+                  imageUrl={category.imageUrl}
+                />
+              );
+            })}
+          </>
+        )}
       </div>
     </div>
   );
