626b37349caa50e2222416d97bfd3bb07a55d723
NaravaTejasri <tejasrisony17@gmail.com>
2022-02-03 22:25:48 +0100

    added validation

--- src/components/Cart/CartdropDown.js
+++ src/components/Cart/CartdropDown.js
@@ -1,17 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { selectCartItem } from "../../store/cart/selector";
 import "../../styles/cart.styles.scss";
 import CustomButton from "../Button/Button";
 import CartItem from "./CartItem";
 
 function CartDropdown() {
+  const items = useSelector(selectCartItem);
   return (
     <div className="cart-dropdown">
       <div className="cart-items">
         <CartItem />
       </div>
       <Link to="/checkout">
-        <CustomButton>GO TO CHECKOUT</CustomButton>
+        {items.length ? <CustomButton>GO TO CHECKOUT</CustomButton> : null}
       </Link>
     </div>
   );
