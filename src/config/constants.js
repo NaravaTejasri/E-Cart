7a0aedbd56c435e84b5a74bdcbdd501a71ce4b3b
NaravaTejasri <tejasrisony17@gmail.com>
2022-02-16 15:56:46 +0100

    added env variables

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,4 +1,3 @@
-export const apiUrl =
-  process.env.API_URL || "https://e-cart-server.herokuapp.com";
+export const apiUrl = process.env.REACT_APP_API_URL || "http://localhost:4000";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
 export const DEFAULT_PAGINATION_LIMIT = 10;
